f279b589c8fdcccea49534478f265397ef14e614
lirond101 <lirond101@gmail.com>
2023-04-30 08:32:42 +0000

    pre-session

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,8 +1,8 @@
-variable "aws_access_key" {
-}
+# variable "aws_access_key" {
+# }
 
-variable "aws_secret_key" {
-}
+# variable "aws_secret_key" {
+# }
 
 variable "aws_region" {
   default = "us-east-1"
@@ -10,6 +10,7 @@ variable "aws_region" {
 
 variable "default_keypair_name" {
   description = "Name of the KeyPair used for all nodes"
+  default = "opsschool-monitoring"
 }
 
 variable "monitor_instance_type" {
@@ -26,5 +27,6 @@ variable "owner" {
 
 variable "vpc_id" {
   description = "ID of vpc to create instances in in the format vpc-xxxxxxxx"
+  default = "vpc-03d6f26d443163293"
 }
 
--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,4 +1,10 @@
 
 terraform {
   required_version = ">= 0.12"
+  cloud {
+    organization = "opsschool-lirondadon"
+    workspaces {
+      name = "opsschool-monitoring"
+    }
+  }
 }
